f65162ca32d01f5cf7c65fe6ee0ad437ae3f04a8
Nunu86 <161253665+Nunu86@users.noreply.github.com>
2024-03-13 14:05:35 +0000

    Made alterations to my HTML to enable accurate timing for selected options

--- src/java.js
+++ src/java.js
@@ -9,12 +9,12 @@ function runDate() {
   americaTime.innerHTML = timeElement;
 
   let dateElement2 = moment().tz(`Europe/Paris`).format(`MMMM Do, YYYY`);
-  let franceDate = document.querySelector(".date2");
+  let franceDate = document.querySelector("#displayDate");
   franceDate.innerHTML = dateElement2;
   let timeElement2 = moment()
     .tz(`Europe/Paris`)
     .format(`h:mm:ss:SS [<small>]A[</small>]`);
-  let franceTime = document.querySelector(".time2");
+  let franceTime = document.querySelector("#displayTime");
   franceTime.innerHTML = timeElement2;
 
   let dateElement3 = moment().tz(`Africa/Lagos`).format(`MMMM Do, YYYY`);
@@ -28,3 +28,25 @@ function runDate() {
 }
 
 setInterval(runDate, 100);
+
+function revealCities(event) {
+  let displayEvent = event.target.value;
+  let countryName = displayEvent.split("/");
+  cityName = countryName[1];
+
+  let cityTimezone = moment().tz(displayEvent);
+  let replaceContent = document.querySelector("#bigContainer");
+
+  replaceContent.innerHTML = ` <div id ="bigContainer">
+          <div class="city-name">
+            ${cityName} 
+          </div>
+          <div class="date"> ${cityTimezone.format("MMMM Do, YYYY")}</div>
+          <div class="time">${cityTimezone.format(
+            "h:mm:ss:SS"
+          )} <small>${cityTimezone.format("A")}</small>)</div> </div>`;
+}
+
+let selectTab = document.querySelector("#cityLabel");
+
+selectTab.addEventListener("change", revealCities);
